b526bca1d4c2d185291d97fd89fa42f6411086a3
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-01-07 11:52:55 +0100

    started working on the player, movement is done. Working on attack animation

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    [SerializeField] private GameObject playerObject;
+
+    private Transform _playerTransform;
+
+    private void Awake()
+    {
+        _playerTransform = playerObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraNewPosition = _playerTransform.position;
+        cameraNewPosition.y += 1;
+        cameraNewPosition.z = -10;
+        transform.position = cameraNewPosition;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    private Transform _transform;
+    
+    
+    // animator variables.
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        int horizontalInput = (int) Input.GetAxisRaw("Horizontal");
+        int verticalInput = (int) Input.GetAxisRaw("Vertical");
+
+        if (verticalInput == 0)
+        {
+            float currentY = rigidbody2D.velocity.y;
+            rigidbody2D.velocity = new Vector2(movementSpeed * horizontalInput, currentY);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(movementSpeed * horizontalInput,movementSpeed * verticalInput);
+        }
+
+        RotatePlayer(horizontalInput);
+        UpdateAnimator(horizontalInput, verticalInput);
+    }
+
+
+    private void RotatePlayer(int horizontalInput)
+    {
+        Vector3 currentScale = _transform.localScale;
+        if (horizontalInput == -1)
+        {
+            currentScale.x = -Math.Abs(transform.localScale.x);
+            _transform.localScale = currentScale;
+        }
+        if (horizontalInput == 1)
+        {
+            currentScale.x = Math.Abs(transform.localScale.x);
+            _transform.localScale = currentScale;            
+        }
+    }
+
+
+    private void UpdateAnimator(int horizontalInput,  int verticalInput)
+    {
+        animator.SetInteger(Horizontal, horizontalInput);
+        animator.SetInteger(Vertical, verticalInput);
+    }
+}
