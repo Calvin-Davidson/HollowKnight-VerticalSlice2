6c422d35bde86efe36996164c288e811ddad66d4
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-01-07 11:58:31 +0100

    added PlayerAttack.cs attack animation is finished, but needs some polishing

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    
+    private static readonly int Attacking = Animator.StringToHash("attacking");
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool(Attacking, true);
+        }
+        else
+        {
+            animator.SetBool(Attacking, false);
+        }
+    }
+}
