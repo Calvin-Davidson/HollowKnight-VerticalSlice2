8643ef56a3ad7eecaa39d78a20d1a5ee906bbcac
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-01-07 12:11:01 +0100

    working on player jump animation, animation look kinda weird

---
+++ Assets/Scripts/Player/PlayerInAir.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInAir : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool isInAir;
+    
+    
+    private static readonly int InAir = Animator.StringToHash("inAir");
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("FloorCollider"))
+        {
+            isInAir = false;
+            animator.SetBool(InAir, false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("FloorCollider"))
+        {
+            isInAir = true;
+            animator.SetBool(InAir, true);
+        }
+    }
+
+
+    public bool IsInAir()
+    {
+        return this.isInAir;
+    }
+}
