3a936dba9260e4501c323e9c11ed560f7a587597
mligtermoet <28470@ma-web.nl>
2021-01-11 13:51:01 +0100

    Camera Movement Updated
    Added Lerp to camera movement & added bounds.

---
+++ Assets/Scripts/Camera/bound1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bound1 : MonoBehaviour
+{
+    [SerializeField] CameraLerp camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Debug.Log("1");
+        camera.changeBound(camera.bounds1);
+    }
+}
