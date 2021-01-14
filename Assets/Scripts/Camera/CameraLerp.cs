7b0cc18f83966e90f86c22f7d0fe8974a63dbf3b
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-01-14 10:02:09 +0100

    fixed camera system, in my branch

--- Assets/Scripts/Camera/CameraLerp.cs
+++ Assets/Scripts/Camera/CameraLerp.cs
@@ -12,11 +12,12 @@ public class CameraLerp : MonoBehaviour
     [SerializeField] public CameraBounds2D bounds1;
     [SerializeField] public CameraBounds2D bounds2;
     [SerializeField] float speed = 3;
-    
+    [SerializeField] private float desiredZposition;
     Vector2 maxXPositions,maxYPositions;
     // Start is called before the first frame update
     void Awake()
-	{
+    {
+	    bounds = bounds1;
 		bounds.Initialize(GetComponent<Camera>());
         	maxXPositions = bounds.maxXlimit;
         	maxYPositions = bounds.maxYlimit;
@@ -37,7 +38,7 @@ public class CameraLerp : MonoBehaviour
         // transform.position = new Vector3(0,newY,-8);
         
          
-         desiredPosition.z = -15;
+         desiredPosition.z = desiredZposition;
          
          
          desiredPosition = new Vector3(Mathf.Clamp(player.position.x, maxXPositions.x, maxXPositions.y), Mathf.Clamp(player.position.y, maxYPositions.x, maxYPositions.y), desiredPosition.z);
